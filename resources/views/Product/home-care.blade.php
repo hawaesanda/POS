45f9e2a7c4aa318d209b37311f3eb0466aaa7d45
hawaesanda <hawa.esanda@gmail.com>
2024-02-29 22:47:00 +0700

    first commit

---
+++ resources/views/Product/home-care.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Care Page</title>
+</head>
+<body>
+    <h1>This is the home page for home care</h1>
+
+    <h2>Home care products:</h2>
+    <ul>
+        <li><strong>Cleaners:</strong> Mr. Clean, Lysol, Wipol.</li>
+        <li><strong>Softeners:</strong> Downy, Molto, Sosoft.</li>
+        <li><strong>Floor cleaners:</strong> So Klin, Wipol, SOS.</li>
+        <li><strong>Home fragrances:</strong> Glade, Stella, Dahlia.</li>
+        <li><strong>Bathroom items :</strong> Towel, Sink, Bucket.</li>
+    </ul>
+</body>
+
+</html>
---
+++ resources/views/Sales/index.blade.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POS Transaction Page</title>
+</head>
+<body>
+    <h1>POS Transaction Page</h1>
+
+    <h2>Transaction Detail</h2>
+    <table border="1">
+        <tr>
+            <th>ID Transaksi</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Total (IDR)</th>
+            <th>Item</th>
+        </tr>
+        <tr>
+            <td>001</td>
+            <td>2024-02-25</td>
+            <td>10:00 AM</td>
+            <td>Rp 100.000,00</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>002</td>
+            <td>2024-02-25</td>
+            <td>02:30 PM</td>
+            <td>Rp 75.500,00</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>003</td>
+            <td>2024-02-25</td>
+            <td>06:45 PM</td>
+            <td>Rp 120.250,00</td>
+            <td>5</td>
+        </tr>
+    </table>
+</body>
+</html>
